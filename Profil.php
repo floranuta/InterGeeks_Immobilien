f0fb590c67e882d62268cc14ce740299b9c2f095
NataliaH <honcharova.de@gmail.com>
2024-12-04 09:44:25 +0100

    .

--- Profil.php
+++ Profil.php
@@ -1,1 +1,37 @@
-/* jhfaskjh*/ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Immobilien</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+
+</head>
+<body>
+    <main class="py-5">
+        <div class="container mb-5">
+            <header class="mb-5">
+              <h1>Kontakt Seite</h1>
+
+            </header>
+
+            <!-- Kontakt Form -->
+            <form action="" method="post">
+                <input type="text" name="login" placeholder="login" required />
+                <input type="password" name="pass" placeholder="pass" required />
+                <input type="email" name="email" placeholder="email" required />
+                <input type="tel" name="tel" placeholder="tel"/>
+
+                <input type="submit" value="Abschicken" />
+          
+            </form>
+
+
+            ?>
+        </div>
+    </main>
+  
+</body>
+</html>
