6ff42e8421bb1cbacb2a913857ad9c9a86296baf
Tetiana Bogach <flora.nuta@gmail.com>
2024-12-04 22:34:32 +0100

    My first commit

---
+++ Beschreibung.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple PHP Page</title>
+</head>
+<body>
+    <?php
+    echo "This is Beschreibung page";
+    ?>
+</body>
+</html>
