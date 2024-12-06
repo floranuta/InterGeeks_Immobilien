ec9f9ecf74766c400a23592a01e5ae1dc63186fa
Elias <Elyas.sadeghi@hotmail.com>
2024-12-06 10:46:35 +0100

    search menu with css

--- Header.php
+++ Header.php
@@ -1,15 +1,6 @@
 <?php
-// connection to the database
-$host = 'localhost';
-$username = 'root';
-$password = 'root';
-$database = 'immobilien_db';
-
-$conn = new mysqli($host, $username, $password, $database);
-
-if ($conn->connect_error) {
-    die('Die Verbindung zur Datenbank konnte nicht aufgebaut werden: ' . $conn->connect_error);
-}
+// include Datenbankverbindung
+include 'dbVerbindung.php';
 ?>
 
 <!DOCTYPE html>
@@ -19,13 +10,24 @@ if ($conn->connect_error) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header</title>
     <link rel='stylesheet' href=css/logo.css>
+    <link rel='stylesheet' href=css/search.css>
 </head>
 <body> 
     <header>
-        <dev class='logo_container'>        
+        <div class='logo_container'>        
             <h1 calss='site_name'>Intergeeks Immobilien</h1>
-            <img src="img/logoo.jpg" alt="Website Logo" class='logo'>
+            <img src="img/Imo.jpg" alt="Website Logo" class='logo'>
         </div>
+        <div class='search'>
+            <form class='search-form' action='search.php' method='post'>
+                <input type='text' id="query" name='query' placeholder='Stadt...' required>
+                <button type='submit'>search</button>
+            </form>
+            <?php
+                include 'search.php';
+            ?>
+        </div>
+                
     </header>  
 </body>
 </html>
