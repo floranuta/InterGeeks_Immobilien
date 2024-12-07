39ad2f6751282cabb59a6b3540ec6fd4b8caa3e9
Elias <Elyas.sadeghi@hotmail.com>
2024-12-07 13:58:00 +0100

    footer with 4 columns

---
+++ Footer/Index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/footer.css">
+</head>
+<body>
+<footer class="footer">
+    <div class="footer-column social-media">
+        <h3>Follow Us</h3>
+        <ul>
+            <li><a href="https://www.facebook.com/">Facebook</a></li>
+            <li><a href="https://x.com/">Twitter</a></li>
+            <li><a href="https://instagram.com/">Instagram</a></li>
+        </ul>
+    </div>
+    
+    <div class="footer-column quick-links">
+        <h3>Quick Links</h3>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Services</a></li>
+        </ul>
+    </div>
+    
+    <div class="footer-column data-protection">
+        <h3>Legal</h3>
+        <ul>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Terms and Conditions</a></li>
+            <li><a href="#">Cookies</a></li>
+        </ul>
+    </div>
+    
+    <div class="footer-column about-us">
+        <h3>About Us</h3>
+        <p>Learn more about our company and mission.</p>
+        <a href="#">Read More</a>
+    </div>
+</footer>
+
+</body>
+</html>
