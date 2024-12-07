d7652db2567c8df7eb696c251875250a70034897
Elias <Elyas.sadeghi@hotmail.com>
2024-12-07 12:43:27 +0100

    new look header, prefinal

--- Header/Index.php
+++ Header/Index.php
@@ -9,30 +9,49 @@ include 'php/db_verbindung.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header</title>
-    <link rel='stylesheet' href=css/logo.css>
-    <link rel='stylesheet' href=css/search.css>
-    <link rel='stylesheet' href=css/anmeldung.css>
+    <!--<link rel='stylesheet' href=css/logo.css>-->
+    <!--<link rel='stylesheet' href=css/search.css>-->
+    <!--<link rel='stylesheet' href=css/anmeldung.css>-->
+    <link rel='stylesheet' href=css/header.css>
 </head>
 <body> 
     <header>
         <div class='logo_container'>        
-            <h1 calss='site_name'>Intergeeks Immobilien</h1>
-            <img src='img/imo.jpg' alt='Website Logo' class='logo'>
+            <!--<h1 calss='site_name'>Intergeeks Immobilien</h1>-->
+            <img src='img/wohnung_logo.jpg' alt='Website Logo' class='logo'>
         </div>
         <div class='search'>
-            <form class='search-form' action='search.php' method='post'>
+            <form class='search-form' action='php/search.php' method='post'>
                 <input type='text' id="query" name='query' placeholder='Stadt...' required>
                 <button type='submit'>search</button>
             </form>
             <?php
                 include 'php/search.php';
-            ?>
+            ?>            
         </div>
+        
         <div class='anmelden'>
-            <img src='img/anmeldung.png' alt='Anmelen hier' class='icon'>
-            <button class='button' onclick='goToLink()'>Klick mich</button>
+            <!--<img src='img/anmeldung.png' alt='Anmelen hier' class='icon'>-->            
+            <a href="https://www.example.com" class="button-link">
+                <button class="button" onclick="navigateTo('Anmelden')">Anmelen</button>
+            </a>
         </div>
-        <script src='js/anmeldung_script.js'></script>      
+         
+        <div>
+            <nav class="nav-buttons">                
+                <a href="https://www.example.com" class="button-link">
+                    <button class="button" onclick="navigateTo('home')">Home</button>
+                </a>
+                
+                <a href="https://www.example.com" class="button-link">
+                    <button class="button" onclick="navigateTo('about-us')">About Us</button>
+                </a>
+                
+                <a href="https://www.example.com" class="button-link">
+                    <button class="button" onclick="navigateTo('contact')">Contact</button>
+                </a>
+            </nav>
+        </div>     
     </header>  
 </body>
 </html>
--- Header/php/search.php
+++ Header/php/search.php
@@ -1,20 +1,21 @@
 <?php 
 // include Datenbankverbindung
-include 'php/db_verbindung.php';
+include 'db_verbindung.php';
  
 // Get the search query
 /*if (!empty($_POST['query'])){
 
 $query = $_POST['query'];}*/
+
 if (isset($_POST['query']) && !empty($_POST['query'])){
   $query=$_POST['query'];
-
 
 // Fetch results
 $sql = "SELECT * FROM wohnungen WHERE Stadt = '".$query."' ";
 $result = $conn->query($sql);
 
 // Display results
+
 if ($result->num_rows > 0) {
   while ($row = $result->fetch_assoc()) {
     echo "<div>" . $row['Stadt'] . "</div>";
